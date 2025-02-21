503c52555ae0667981dea0aac25945ffbda8fcd6
Cadu <carlos.e.bueno@hotmail.com>
2025-02-21 14:02:55 -0300

    'teste2'

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 string apiKey = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"; // Exemplo de chave API
+string apiKey = "sk_test_4eC39HqLyjWDarjtT1zdp241"; // Exemplo de chave API
